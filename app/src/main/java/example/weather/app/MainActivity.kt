1b4488ecf38fc1ebd213aa80e363f1c695c66b70
ultron <ultron.starkson.55@gmail.com>
2024-08-16 21:19:48 +0500

    main

--- app/src/main/java/example/weather/app/MainActivity.kt
+++ app/src/main/java/example/weather/app/MainActivity.kt
@@ -5,11 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import example.weather.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .add(binding.main.id, MainFragment())
+            .commit()
     }
 }
